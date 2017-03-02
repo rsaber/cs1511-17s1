80448481df7ae27fd8702fe7c12266462906dcd2
Riyasat Saber <r.saber@unsw.edu.au>
2017-03-02 21:56:55 +1100

    Tute1 Material Added

---
+++ tute1/face.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	printf("~ ~\n0 0\n O \n\\-/\n");
+	return 0;
+}
---
+++ tute1/hello.c
@@ -0,0 +1,6 @@
+// This program prints out "hello h15b"
+// written by john
+
+#include <stdio.h>
+
+int main(){printf("Hello H15B\\n");return 0;}
