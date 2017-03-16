880bf45f894c7505c2aa888f1b49c3fe05fd9137
Riyasat Saber <r.saber@unsw.edu.au>
2017-03-16 13:19:28 +1100

    q1 q2 done

---
+++ tute3/assign.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	int j = 0;
+	printf("%d\n", j = 1);
+	return 0;
+}
---
+++ tute3/asterisk.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int lines, i=0;
+	scanf("%d", &lines);
+	while(i < lines){
+		printf("*\n");
+		i++;
+	}
+	return 0;
+	
+}
