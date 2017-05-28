b6bcc65434c55b281089e021514eefe7894fe438
Riyasat Saber <riyasatsaber@hotmail.com>
2017-05-28 22:17:02 +1000

    Dodgy tests added

---
+++ sample_pracs/linked_lists/test.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <assert.h>
+#include 'list.h'
+
+/*
+  Linked List sample prac exam
+  Written by Riyasat Saber
+  for COMP1511 17s1
+*/
+
+void test_append(){
+  
+}
+
+void test_delete(){
+
+}
+
+void test_duplicateList(){
+
+}
+
+void test_get(){
+
+}
+
+void test_getFromEnd(){
+
+}
+
+void test_reverse(){
+
+}
+
+void test_inplaceMerge(){
+
+}
+
+void test_isCycle(){
+
+}
+
+void test_pairwiseSwap(){
+
+}
+
+void test_zip(){
+
+}
+
+void test_sum(){
+
+}
+
+int main(){
+
+}
